341aed3bc339fc87a80a7ef552d67b5dd49c93c3
hzy <1396510655@qq.com>
2020-04-02 22:12:36 +0800

    update 添加组件示例

---
+++ HZY.Admin/Controllers/Base/OtherController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HZY.Admin.Controllers.Base
+{
+    public class OtherController : ApiBaseController
+    {
+
+
+        #region 页面 Views
+
+        [HttpGet(nameof(Index))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+
+
+    }
+}
